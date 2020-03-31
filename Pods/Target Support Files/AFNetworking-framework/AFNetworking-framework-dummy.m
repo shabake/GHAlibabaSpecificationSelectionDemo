a2530a012a869a0873b3c1142ab1969a24024567
shabake <45329453@qq.com>
2020-03-31 11:31:34 +0800

    更新README

--- Pods/Target Support Files/AFNetworking-framework/AFNetworking-framework-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_AFNetworking_framework : NSObject
-@end
-@implementation PodsDummy_AFNetworking_framework
-@end
--- Pods/Target Support Files/YYModel-library/YYModel-library-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_YYModel_library : NSObject
-@end
-@implementation PodsDummy_YYModel_library
-@end
